27d737557b183870add0b0e01a8741977af8ab96
Psoewish <psoewish@proton.me>
2025-11-23 07:58:27 +0100

    Some fileflows fixes

--- profiles/homelab/media-services/fileflows.nix
+++ profiles/homelab/media-services/fileflows.nix
@@ -22,11 +22,12 @@ lib.mkMerge [
     virtualisation.oci-containers.containers.fileflows = {
       image = "revenz/fileflows:latest";
       volumes = [
-        "/var/lib/fileflows:/root/.fileflows:Z"
-        "/var/log/fileflows:/root/Logs:Z"
-        "/tmp/fileflows:/tmp:Z"
+        "/var/lib/fileflows:/app/Data:Z"
+        "/var/log/fileflows:/app/Logs:Z"
+        "/tmp/fileflows:/temp:Z"
         "/data/media:/media:Z"
         "/var/run/podman/podman.sock:/var/run/docker.sock:ro,Z"
+        "/dev/dri:/dev/dri:Z"
       ];
       user = "0:${toString config.users.groups.media.gid}";
       extraOptions = [
@@ -39,7 +40,7 @@ lib.mkMerge [
       "Z /var/lib/fileflows - - - - -"
       "d /var/log/fileflows 0775 ${fileflowsUser} media -"
       "Z /var/log/fileflows - - - - -"
-      "d /tmp/fileflows 0750 ${fileflowsUser} media -"
+      "d /tmp/fileflows 0775 ${fileflowsUser} media -"
       "Z /tmp/fileflows - - - - -"
     ];
   }
