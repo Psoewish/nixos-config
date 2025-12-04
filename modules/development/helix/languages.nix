d8957fcb844c34ab7eb6d9cf59e6ff9cd724344d
Psoewish <psoewish@proton.me>
2025-12-04 10:08:14 +0100

    Remove brittle nixd implementation
    Just gonna run it "raw" for a while, and see if I actually need the
    fancy configuration. But it would constantly stall helix on trying to
    format when something, somewhere, had a syntax error, lol.

--- modules/development/helix/languages.nix
+++ modules/development/helix/languages.nix
@@ -1,66 +1,56 @@
 { username, ... }:
 {
-  home-manager.users.${username} =
-    { osConfig, ... }:
-    {
-      programs.helix = {
-        languages = {
-          language = [
-            {
-              name = "nix";
-              auto-format = true;
-            }
-            {
-              name = "lua";
-              auto-format = true;
-            }
-            {
-              name = "markdown";
-              language-servers = [
-                "marksman"
-                "markdown-oxide"
-                "harper-ls"
-                "mpls"
-              ];
-              formatter = {
-                command = "prettier";
-                args = [
-                  "--parser"
-                  "--markdown"
-                  "--prose-wrap"
-                  "never"
-                ];
-              };
-              auto-format = true;
-            }
-          ];
-          language-server.nixd = {
-            command = "nixd";
-            config.nixd =
-              let
-                myFlake = ''(builtins.getFlake "${osConfig.programs.nh.flake}")'';
-                nixosOpts = "${myFlake}.nixosConfigurations.${osConfig.networking.hostName}.options";
-              in
-              {
-                formatting.command = [ "nixfmt" ];
-                nixpkgs.expr = "import ${myFlake}.inputs.nixpkgs { }";
-                options.home-manager.expr = nixosOpts;
-                options.nixos.expr = "${nixosOpts}.home-manager.users.type.getSubOptions []";
-              };
-          };
-          language-server.harper-ls = {
-            command = "harper-ls";
-            args = [ "--stdio" ];
-          };
-          language-server.mpls = {
-            command = "mpls";
-            args = [
-              "--dark-mode"
-              "--enable-emoji"
-              "--full-sync"
+  home-manager.users.${username} = {
+    programs.helix = {
+      languages = {
+        language = [
+          {
+            name = "nix";
+            language-servers = [
+              "nixd"
             ];
-          };
+            auto-format = true;
+            formatter = {
+              command = "nixfmt";
+            };
+          }
+          {
+            name = "lua";
+            auto-format = true;
+          }
+          {
+            name = "markdown";
+            language-servers = [
+              "marksman"
+              "markdown-oxide"
+              "harper-ls"
+              "mpls"
+            ];
+            formatter = {
+              command = "prettier";
+              args = [
+                "--parser"
+                "--markdown"
+                "--prose-wrap"
+                "never"
+              ];
+            };
+            auto-format = true;
+          }
+        ];
+        language-server.harper-ls = {
+          command = "harper-ls";
+          args = [ "--stdio" ];
+        };
+        language-server.mpls = {
+          command = "mpls";
+          args = [
+            "--dark-mode"
+            "--enable-emoji"
+            "--full-sync"
+          ];
         };
       };
     };
+  };
 }
