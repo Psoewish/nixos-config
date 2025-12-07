c775859ee3c912acc3765b5a5484ac9e28ac0c99
Psoewish <psoewish@proton.me>
2025-12-07 19:25:01 +0100

    Add Zen plugins

---
+++ modules/desktop/zen/default.nix
@@ -0,0 +1,33 @@
+{ username, ... }:
+{
+  home-manager.users.${username} =
+    { inputs, pkgs, ... }:
+    {
+      imports = [ inputs.zen-browser.homeModules.twilight ];
+
+      programs.zen-browser = {
+        enable = true;
+        profiles."default" = {
+          extensions.packages =
+            with inputs.zen-browser-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
+              clearurls
+              indie-wiki-buddy
+              lovely-forks
+              no-pdf-download
+              pronoundb
+              proton-pass
+              protondb-for-steam
+              privacy-badger
+              return-youtube-dislikes
+              dearrow
+              augmented-steam
+              sponsorblock
+              istilldontcareaboutcookies
+              refined-github
+              sonarr-radarr-lidarr-search
+              unpaywall
+            ];
+        };
+      };
+    };
+}
