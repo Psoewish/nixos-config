27dde6f3aff28d2434105756040776ceb3d98082
Psoewish <psoewish@proton.me>
2025-12-08 09:44:25 +0100

    Basic wezterm setup

--- modules/desktop/wezterm/default.nix
+++ modules/desktop/wezterm/default.nix
@@ -1,8 +1,27 @@
 { username, ... }:
 {
   home-manager.users.${username} = {
+    catppuccin.wezterm.enable = false;
     programs.wezterm = {
       enable = true;
+      extraConfig = /* lua */ ''
+        local wezterm = require 'wezterm'
+        local config = wezterm.config_builder()
+
+        config = {
+          color_scheme = "Catppuccin Mocha",
+          use_fancy_tab_bar = false,
+          hide_tab_bar_if_only_one_tab = true,
+          window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+          },
+        }
+
+        return config
+      '';
     };
   };
 }
