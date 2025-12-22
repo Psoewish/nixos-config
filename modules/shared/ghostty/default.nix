88114e7f6637aa03281777980e05a4b0890444a3
Psoewish <personal@psoewish.com>
2025-12-22 12:16:08 +0100

    Add ghostty and helix to shared

---
+++ modules/shared/ghostty/default.nix
@@ -0,0 +1,22 @@
+{ username, ... }:
+{
+  home-manager.users.${username} = {
+    programs.ghostty = {
+      enable = true;
+      settings = {
+        command = "fish";
+        font-family = "CaskaydiaCove Nerd Font";
+        font-size = 12;
+        theme = "Soft Server";
+        clipboard-read = "allow";
+        clipboard-write = "allow";
+        copy-on-select = true;
+        right-click-action = "copy-or-paste";
+        confirm-close-surface = false;
+        window-inherit-working-directory = false;
+        quit-after-last-window-closed = true;
+        keybind = "shift+enter=text:\\x1b\\r";
+      };
+    };
+  };
+}
