a2b2e668974549c359e0c030ebf4c744734fa529
Psoewish <personal@psoewish.com>
2026-01-14 01:15:37 +0100

    Update Caddy config

--- modules/homelab/caddy/default.nix
+++ modules/homelab/caddy/default.nix
@@ -13,20 +13,37 @@
     };
     email = "admin@psoewish.com";
     environmentFile = config.sops.secrets."cloudflared/api".path;
-    globalConfig = ''
+    globalConfig = /* Caddyfile */ ''
       acme_dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+
       servers {
         protocols h1 h2 h3
+        trusted_proxies static private_ranges
+        client_ip_headers CF-Connecting-IP X-Forwarded-For X-Real-IP
       }
+
     '';
     extraConfig = ''
-      (secure_headers) {
-        header Strict-Transport-Security "max-age=31536000; includeSubDomains"
-        header X-Frame-Options "SAMEORIGIN"
-        header X-Content-Type-Options "nosniff"
-        header X-XSS-Protection "1; mode=block"
-        header Referrer-Policy "strict-origin-when-cross-origin"
-        header X-Forwarded-Proto "https"
+      (security_defaults) {
+        encode zstd gzip
+
+        header / {
+          Strict-Transport-Security "max-age=31536000; includeSubDomains"
+          X-Frame-Options "SAMEORIGIN"
+          X-Content-Type-Options "nosniff"
+          X-Robots-Tag "noindex, nofollow"
+          X-XSS-Protection "1; mode=block"
+          Referrer-Policy "strict-origin-when-cross-origin"
+          X-Forwarded-Proto "https"
+        }
+      }
+
+      (admin_redir) {
+        @admin {
+          path /admin*
+          not remote_ip private_ranges
+        }
+        redir @admin /
       }
     '';
 
@@ -35,7 +52,8 @@
         "${cfg.subdomain}.${config.homelab.domain}" = {
           serverAliases = cfg.aliases;
           extraConfig = ''
-            import secure_headers
+            import security_defaults
+            import admin_redir
             reverse_proxy localhost:${toString cfg.port}
           '';
         };
