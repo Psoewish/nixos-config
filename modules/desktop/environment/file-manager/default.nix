224ee67bdd4c5836aa880191cd32d5c7013d14f4
Psoewish <personal@psoewish.com>
2026-02-10 19:02:12 +0100

    Remove nautilus and loupe explicit install (let gnome handle it implicitly)

--- modules/desktop/environment/file-manager/default.nix
+++ modules/desktop/environment/file-manager/default.nix
@@ -1,10 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  environment.systemPackages = with pkgs; [
-    nautilus
-    loupe
-  ];
-
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "ghostty";
