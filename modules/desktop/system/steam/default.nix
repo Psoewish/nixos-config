1bd734c759b27767c83239d521e15462bfc7b77b
Psoewish <personal@psoewish.com>
2026-02-13 13:59:48 +0100

    Fix proton/steam timezone issues

--- modules/desktop/system/steam/default.nix
+++ modules/desktop/system/steam/default.nix
@@ -2,8 +2,17 @@
 {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        TZDIR = "/usr/share/zoneinfo";
+      };
+    };
+    remotePlay.openFirewall = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
+    ];
+    extraPackages = with pkgs; [
+      adwsteamgtk
     ];
     protontricks.enable = true;
   };
