895205ed53e7bed2c4f68a6213716402866310c3
Psoewish <personal@psoewish.com>
2026-02-27 10:17:58 +0100

    Some small cosmic tweaks

--- modules/desktop/environment/cosmic/default.nix
+++ modules/desktop/environment/cosmic/default.nix
@@ -10,25 +10,17 @@
     };
 
     desktopManager.cosmic.enable = true;
-    flatpak.enable = true;
   };
 
-  environment = {
-    sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
-
-    systemPackages = with pkgs; [
-      # Cosmic tweaks/utils/applets
-      cosmic-ext-tweaks
-      cosmic-ext-calculator
-      cosmic-ext-applet-weather
-      cosmic-ext-applet-caffeine
-      cosmic-ext-applet-privacy-indicator
-
-      # Random apps made for cosmic
-      bazaar
-      tasks
-      forecast
-      quick-webapps
-    ];
-  };
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+  environment.systemPackages = with pkgs; [
+    cosmic-ext-tweaks
+    cosmic-ext-calculator
+    cosmic-ext-applet-weather
+    cosmic-ext-applet-caffeine
+    cosmic-ext-applet-privacy-indicator
+    quick-webapps
+    resources
+    loupe
+  ];
 }
