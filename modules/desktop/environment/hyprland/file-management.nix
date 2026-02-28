0d31baf57cee2d5e2f513e60ace620e152c20c98
Psoewish <personal@psoewish.com>
2026-02-28 17:21:13 +0100

    Fix typo

--- modules/desktop/environment/hyprland/file-management.nix
+++ modules/desktop/environment/hyprland/file-management.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = [ pkgs.nautilus ];
 
-  programs.nautilus.open-any-terminal = {
+  programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "ghostty";
   };
