fb2eb0464725f61a829898fa0c129ea6cb11f945
Psoewish <personal@psoewish.com>
2026-04-21 04:36:57 +0200

    Adapt filesystem to new install

--- hosts/desktop/filesystem.nix
+++ hosts/desktop/filesystem.nix
@@ -29,7 +29,7 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7147-65F3";
+    { device = "/dev/disk/by-uuid/C909-FBEB";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
