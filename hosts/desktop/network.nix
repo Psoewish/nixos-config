1304aaccf5f801fab53388d803e3c2bf4598f957
Psoewish <personal@psoewish.com>
2026-06-02 17:57:35 +0200

    Update ssh settings

--- hosts/desktop/network.nix
+++ hosts/desktop/network.nix
@@ -1,4 +1,4 @@
-{ username, ... }:
+{ ... }:
 {
   networking = {
     networkmanager = {
@@ -7,16 +7,12 @@
     nftables.enable = true;
   };
 
-  services.openssh.enable = true;
-  home-manager.users.${username} = {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      matchBlocks = {
-        "homelab" = {
-          hostname = "192.168.1.100";
-          user = "root";
-        };
+  services.openssh = {
+    enable = true;
+    knownHosts = {
+      homelab = {
+        extraHostNames = [ "192.168.1.100" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOAKGYMNaCK17QYNwe4PUJ+6INU303baKyMqzYnpMS5R";
       };
     };
   };
