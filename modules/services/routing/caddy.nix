865298793585c6acda5f6e7f738273c95cbf2fe9
Psoewish <personal@psoewish.com>
2026-07-14 03:16:17 +0200

    Update Caddy plugin hash

--- modules/services/routing/caddy.nix
+++ modules/services/routing/caddy.nix
@@ -9,7 +9,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-Gb1nC5fZfj7IodQmKmEPGygIHNYhKWV1L0JJiqnVtbs=";
+      hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
     };
     email = "admin@psoewish.com";
     environmentFile = config.sops.secrets."cloudflared/api".path;
