4a07e6bf9823e404ab48882d55c81a5181fd33bf
Psoewish <personal@psoewish.com>
2026-07-23 06:19:36 +0200

    Format flake.nix a lil' better

--- flake.nix
+++ flake.nix
@@ -10,9 +10,9 @@
         systems.hosts = {
           desktop.tags = [
             core
+            secrets
             environment
             theming
-            secrets
           ];
           homelab.tags = [
             core
@@ -25,20 +25,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     hjem.url = "github:feel-co/hjem";
     hjem.inputs.nixpkgs.follows = "nixpkgs";
+
     noctalia.url = "github:noctalia-dev/noctalia";
     noctalia.inputs.nixpkgs.follows = "nixpkgs";
+
     catppuccin.url = "github:catppuccin/nix";
+
     helium.url = "github:oxcl/nix-flake-helium-browser";
     helium.inputs.nixpkgs.follows = "nixpkgs";
+
     streamcontroller.url = "github:daaboulex/streamcontroller-nix";
     streamcontroller.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-flatpak.url = "github:gmodena/nix-flatpak/latest";
+
     xcaddy-nix.url = "github:michailik/nix-caddy-withplugins";
   };
 }
