8db9d410da36bbdcdf0fca26c4c8a26de32e6828
Psoewish <personal@psoewish.com>
2026-08-02 15:49:30 +0200

    Add cosmic

---
+++ modules/sessions/cosmic/cosmic.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.cosmic = { inputs, pkgs, ... }: {
+    imports = [ inputs.self.modules.nixos.greeter ];
+
+    services.desktopManager.cosmic.enable = true;
+
+    environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+    environment.systemPackages = with pkgs; [
+      cosmic-ext-calculator
+      cosmic-ext-applet-caffeine
+      cosmic-ext-applet-privacy-indicator
+      cosmic-monitor
+      loupe
+    ];
+  };
+}
