cd5cc080292eafb7cda88389f23510f6cc39e005
Psoewish <personal@psoewish.com>
2026-08-02 14:13:59 +0200

    Add ghostty

---
+++ modules/programs/ghostty/default.nix
@@ -0,0 +1,12 @@
+{
+  flake.modules.nixos.ghostty = { inputs, ... }: {
+    home-manager.sharedModules = [ inputs.self.modules.homeManager.ghostty ];
+  };
+
+  flake.modules.homeManager.ghostty = {
+    programs.ghostty = {
+      enable = true;
+      systemd.enable = true;
+    };
+  };
+}
