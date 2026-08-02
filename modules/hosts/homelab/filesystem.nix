06e33d81f594775340f1831a0a4b149e735f5877
Psoewish <personal@psoewish.com>
2026-08-02 16:12:27 +0200

    Add (basic) homelab host

---
+++ modules/hosts/homelab/filesystem.nix
@@ -0,0 +1,40 @@
+{
+  flake.modules.nixos.homelab = { pkgs, ... }: {
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/ad374118-5ab8-4e31-97f9-0e5e8f9de75b";
+        fsType = "ext4";
+      };
+
+      "/boot" = {
+        device = "/dev/disk/by-uuid/5582-FDA7";
+        fsType = "vfat";
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
+      };
+    };
+
+    swapDevices = [ { device = "/dev/disk/by-uuid/4071aba9-4533-4819-b234-a8903e58a990"; } ];
+
+    services.zfs = {
+      autoScrub.enable = true;
+      autoScrub.interval = "monthly";
+      trim.enable = true;
+    };
+
+    networking.hostId = "f46ba50b";
+    boot = {
+      supportedFilesystems.zfs = true;
+      zfs = {
+        package = config.boot.kernelPackages.zfs_cachyos;
+        extraPools = [
+          "speedy"
+          "rusty"
+        ];
+        forceImportRoot = false;
+      };
+    };
+  };
+}
