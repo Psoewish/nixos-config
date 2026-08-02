f4a69b35c06422aefe7b3300dd3854b54118c680
Psoewish <personal@psoewish.com>
2026-08-02 14:14:28 +0200

    Add locale, network, virtualisation modules

---
+++ modules/system/network.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules.nixos.locale = { config, ... }: {
+    networking = {
+      networkmanager = {
+        enable = true;
+        dns = "none";
+      };
+      useDHCP = false;
+      dhcpcd.enable = false;
+      nftables.enable = true;
+      nameservers = [
+        config.constants.hosts.homelab.staticIp
+        "9.9.9.9"
+      ];
+      defaultGateway = "192.168.1.1";
+    };
+  };
+}
---
+++ modules/system/virtualisation.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.nixos.virtualisation = { pkgs, config, ... }: {
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu.swtpm.enable = true;
+      };
+      spiceUSBRedirection.enable = true;
+      docker.enable = true;
+    };
+
+    users.groups = {
+      libvirtd.members = [ config.constants.primaryUser ];
+      kvm.members = [ config.constants.primaryUser ];
+      docker.members = [ config.constants.primaryUser ];
+    };
+
+    environment.systemPackages = with pkgs; [
+      gnome-boxes
+      dnsmasq
+      phodav
+      docker-compose
+    ];
+  };
+}
