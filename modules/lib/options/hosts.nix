b617608eed33c693c506a9ec99925360ae212039
psoewish <personal@psoewish.com>
2026-08-07 07:16:16 +0200

    Add a primaryUser host option

--- modules/lib/options/hosts.nix
+++ modules/lib/options/hosts.nix
@@ -2,12 +2,29 @@
   options.flake.hosts = lib.mkOption {
     type = lib.types.attrsOf (lib.types.attrsOf (lib.types.submoduleWith {
       modules = [
-        ({name, ...}: {
+        ({
+          name,
+          config,
+          ...
+        }: {
           freeformType = lib.types.attrsOf lib.types.unspecified;
           options = {
             hostname = lib.mkOption {
               type = lib.types.str;
               default = name;
+            };
+            primaryUser = lib.mkOption {
+              type = lib.types.str;
+              default = let
+                primary = lib.attrNames (lib.filterAttrs (_: u: u.isPrimary) config.users);
+              in
+                if primary == []
+                then
+                  throw
+                  "Host '${name}' has no primary user defined"
+                else if lib.length primary > 1
+                then throw "Host '${name}' has multiple primary users defines: ${toString primary}"
+                else lib.head primary;
             };
             system = lib.mkOption {type = lib.types.str;};
             stateVersion = lib.mkOption {type = lib.types.str;};
