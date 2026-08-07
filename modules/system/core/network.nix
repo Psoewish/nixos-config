38e9afced5cfd73fd5dbcc523146f4e541f3fe3a
psoewish <personal@psoewish.com>
2026-08-07 16:20:24 +0200

    Fix reference

--- modules/system/core/network.nix
+++ modules/system/core/network.nix
@@ -9,7 +9,7 @@
       dhcpcd.enable = false;
       nftables.enable = true;
       nameservers = [
-        config.constants.hosts.homelab.staticIp
+        config.hosts.homelab.staticIp
         "9.9.9.9"
       ];
       defaultGateway = "192.168.1.1";
