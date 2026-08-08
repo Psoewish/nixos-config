7e41613dc3e0ed4ecac436ac48f014267b38a7cd
psoewish <personal@psoewish.com>
2026-08-08 11:52:13 +0200

    Port sabnzbd to native service

--- modules/services/backend/sabnzbd.nix
+++ modules/services/backend/sabnzbd.nix
@@ -1,19 +1,9 @@
 {
-  flake.modules.nixos.sabnzbd = {config, ...}: {
-    virtualisation.oci-containers.containers.sabnzbd = {
-      hostname = "sabnzbd";
-      image = "lscr.io/linuxserver/sabnzbd:latest";
-      pull = "always";
-      environment = {
-        PUID = toString config.hosts.homelab.users.media.id;
-        PGID = toString config.hosts.homelab.users.media.id;
-        TZ = config.time.timeZone;
-      };
-      volumes = [
-        "/var/lib/sabnzbd:/config"
-        "/data/downloads/usenet:/downloads/usenet"
-      ];
-      extraOptions = ["--network=host"];
+  flake.modules.nixos.sabnzbd = {
+    services.sabnzbd = {
+      enable = true;
+      group = "media";
+      configFile = "/var/lib/sabnzbd/sabnzbd.ini"; # Add full settings later
     };
   };
 
