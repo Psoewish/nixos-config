1399e4b08d5dbc7890db3577244ef2aa6e79265c
psoewish <personal@psoewish.com>
2026-08-08 09:40:45 +0200

    Port nzbhydra2 to native version

--- modules/services/backend/nzbhydra.nix
+++ modules/services/backend/nzbhydra.nix
@@ -1,18 +1,7 @@
 {
-  flake.modules.nixos.nzbhydra = {config, ...}: {
-    virtualisation.oci-containers.containers.nzbhydra = {
-      hostname = "nzbhydra";
-      image = "lscr.io/linuxserver/nzbhydra2:latest";
-      pull = "always";
-      environment = {
-        PUID = toString config.hosts.homelab.users.media.id;
-        PGID = toString config.hosts.homelab.users.media.id;
-        TZ = config.time.timeZone;
-      };
-      volumes = [
-        "/var/lib/nzbhydra2:/config"
-      ];
-      extraOptions = ["--network=host"];
+  flake.modules.nixos.nzbhydra = {
+    services.nzbhydra2 = {
+      enable = true;
     };
   };
 
