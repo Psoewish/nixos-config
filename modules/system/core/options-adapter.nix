48053e72690187765ed5291cbd444ddf0395d406
psoewish <personal@psoewish.com>
2026-08-08 08:28:55 +0200

    Tweak route propagation a tiny bit.

--- modules/system/core/options-adapter.nix
+++ modules/system/core/options-adapter.nix
@@ -6,17 +6,19 @@
     routes,
     ...
   }: {
-    options.hosts = lib.mkOption {
-      type = lib.types.attrsOf lib.types.unspecified;
-      default = hosts;
-    };
-    options.global = lib.mkOption {
-      type = lib.types.attrsOf lib.types.unspecified;
-      default = global;
-    };
-    options.routes = lib.mkOption {
-      type = lib.types.attrsOf lib.types.unspecified;
-      default = routes;
+    options = {
+      hosts = lib.mkOption {
+        type = lib.types.attrsOf lib.types.unspecified;
+        default = hosts;
+      };
+      global = lib.mkOption {
+        type = lib.types.attrsOf lib.types.unspecified;
+        default = global;
+      };
+      routes = lib.mkOption {
+        type = lib.types.attrsOf lib.types.unspecified;
+        default = routes;
+      };
     };
   };
 }
