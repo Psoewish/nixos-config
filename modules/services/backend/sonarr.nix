ccb71d78d056c59b3443771fbb9dc4e8a94a5e9d
psoewish <personal@psoewish.com>
2026-08-08 11:52:23 +0200

    Port sonarr to native service

--- modules/services/backend/sonarr.nix
+++ modules/services/backend/sonarr.nix
@@ -1,21 +1,9 @@
 {
-  flake.modules.nixos.sonarr = {config, ...}: {
-    virtualisation.oci-containers.containers.sonarr = {
-      hostname = "sonarr";
-      image = "lscr.io/linuxserver/sonarr:latest";
-      pull = "always";
-      environment = {
-        PUID = toString config.hosts.homelab.users.media.id;
-        PGID = toString config.hosts.homelab.users.media.id;
-        TZ = config.time.timeZone;
-      };
-      volumes = [
-        "/var/lib/sonarr:/config"
-        "/data/media/tv:/tv"
-        "/data/downloads/usenet:/downloads/usenet"
-        "/data/downloads/torrents:/downloads/torrents"
-      ];
-      extraOptions = ["--network=host"];
+  flake.modules.nixos.sonarr = {
+    services.sonarr = {
+      enable = true;
+      group = "media";
+      dataDir = "/var/lib/sonarr";
     };
   };
 
