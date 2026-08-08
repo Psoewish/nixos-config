27e56c1f0bc68e4dae8cb863178f7a3a3d84d680
psoewish <personal@psoewish.com>
2026-08-08 04:25:01 +0200

    Fix vaultwarden

--- modules/services/frontend/vaultwarden.nix
+++ modules/services/frontend/vaultwarden.nix
@@ -6,10 +6,13 @@
         config.age.secrets.vaultwarden_admin_token.path
         config.age.secrets.vaultwarden_smtp_password.path
       ];
-      domain = "https://vault.${config.global.domain}";
+      domain = "vault.${config.global.domain}";
       config = {
+        USE_SYSLOG = "true";
+        EXTENDED_LOGGING = "true";
         SIGNUPS_ALLOWED = "false";
         WEB_VAULT_ENABLED = "true";
+        ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = "8222";
         SMTP_HOST = "smtp.fastmail.com";
         SMTP_PORT = "587";
