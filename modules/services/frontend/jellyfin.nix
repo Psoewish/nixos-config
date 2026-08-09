8db378e5bff548fdf99caba67727a65659ae6cc4
psoewish <personal@psoewish.com>
2026-08-09 14:46:41 +0200

    Port jellyfin to native service

--- modules/services/frontend/jellyfin.nix
+++ modules/services/frontend/jellyfin.nix
@@ -1,21 +1,9 @@
 {
-  flake.modules.nixos.jellyfin = {config, ...}: {
-    virtualisation.oci-containers.containers.jellyfin = {
-      hostname = "jellyfin";
-      image = "jellyfin/jellyfin:latest";
-      pull = "always";
-      environment = {
-        PUID = toString config.hosts.homelab.users.media.id;
-        PGID = toString config.hosts.homelab.users.media.id;
-        TZ = config.time.timeZone;
-      };
-      volumes = [
-        "/var/lib/jellyfin:/config"
-        "/data/media/tv:/data/media/tv"
-        "/data/media/movies:/data/media/movies"
-      ];
-      devices = ["/dev/dri:/dev/dri"];
-      extraOptions = ["--network=host"];
+  flake.modules.nixos.jellyfin = {
+    services.jellyfin = {
+      enable = true;
+      group = "media";
+      dataDir = "/var/lib/jellyfin";
     };
   };
 
