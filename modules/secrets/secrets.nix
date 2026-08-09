cf59f0e6ad87e1e2c2f347d1bd93168c3a898fd4
psoewish <personal@psoewish.com>
2026-08-09 12:43:44 +0200

    Add sabnzbd secrets + declarative config

--- modules/secrets/secrets.nix
+++ modules/secrets/secrets.nix
@@ -8,6 +8,10 @@
       sonarr_api.rekeyFile = ./sonarr_api.age;
       radarr_api.rekeyFile = ./radarr_api.age;
       prowlarr_api.rekeyFile = ./prowlarr_api.age;
+      sabnzbd_secrets = {
+        rekeyFile = ./sabnzbd_secrets.age;
+        owner = "sabnzbd";
+      };
 
       vaultwarden_admin_token.rekeyFile = ./vaultwarden_admin_token.age;
       vaultwarden_smtp_password.rekeyFile = ./vaultwarden_smtp_password.age;
--- modules/services/backend/sabnzbd.nix
+++ modules/services/backend/sabnzbd.nix
@@ -1,9 +1,26 @@
 {
-  flake.modules.nixos.sabnzbd = {
+  flake.modules.nixos.sabnzbd = {config, ...}: {
     services.sabnzbd = {
       enable = true;
       group = "media";
-      configFile = "/var/lib/sabnzbd/sabnzbd.ini"; # Add full settings later
+      allowConfigWrite = true;
+      secretFiles = [config.age.secrets.sabnzbd_secrets.path];
+      settings = {
+        misc = {
+          download_dir = "/data/downloads/usenet/incomplete";
+          complete_dir = "/data/downloads/usenet/complete";
+          host_whitelist = ["localhost" "${config.routes.sabnzbd.service}.${config.global.domain}"];
+        };
+        categories = {
+          movies.name = "movies";
+          tv.name = "tv";
+          software.name = "software";
+          music.name = "music";
+          books.name = "books";
+          manual.name = "manual";
+        };
+      };
+      # configFile = "/var/lib/sabnzbd/sabnzbd.ini"; # Add full settings later
     };
   };
 
