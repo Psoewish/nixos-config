8af421d029bbd2133b9ed47fb9d77a6d59948ca1
psoewish <personal@psoewish.com>
2026-08-11 18:30:40 +0200

    Fix firefox config

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -1,43 +1,60 @@
 {
   flake-file.inputs = {
-    firefox-extensions.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-    firefox-extensions.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/nur";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   flake.modules.nixos.firefox = {inputs, ...}: {
+    imports = [inputs.nur.modules.nixos.default];
     home-manager.sharedModules = [inputs.self.modules.homeManager.firefox];
   };
 
   flake.modules.homeManager.firefox = {
-    inputs,
     pkgs,
+    config,
     ...
   }: {
     programs.firefox = {
       enable = true;
+      configPath = "${config.xdg.configHome}/mozilla/firefox";
       profiles.default = {
+        name = "default";
+        id = 0;
+        isDefault = true;
         settings = {
           "extensions.autoDisableScopes" = 0;
           "sidebar.verticalTabs" = true;
+          "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+          "browser.startup.page" = 3;
+          "browser.ai.control.default" = "blocked";
+          "browser.ai.control.linkPreviewKeyPoints" = "blocked";
+          "browser.ai.control.pdfjsAltText" = "blocked";
+          "browser.ai.control.sidebarChatbot" = "blocked";
+          "browser.ai.control.smartTabGroups" = "blocked";
+          "browser.ai.control.smartWindow" = "blocked";
+          "browser.ai.control.translations" = "blocked";
+          "browser.dataFeatureRecommendations.enabled" = false;
+          "browser.discovery.containers.enabled" = false;
         };
         extensions = {
           force = true;
-          packages = with inputs.firefox-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
+          packages = with pkgs.nur.repos.rycee.firefox-addons; [
+            augmented-steam
+            bitwarden
             clearurls
+            frankerfacez
             indie-wiki-buddy
+            istilldontcareaboutcookies
             lovely-forks
             no-pdf-download
+            privacy-badger
             pronoundb
             protondb-for-steam
-            privacy-badger
-            return-youtube-dislikes
-            augmented-steam
-            sponsorblock
-            istilldontcareaboutcookies
             refined-github
+            return-youtube-dislikes
             sonarr-radarr-lidarr-search
+            sponsorblock
             unpaywall
-            bitwarden
           ];
         };
       };
