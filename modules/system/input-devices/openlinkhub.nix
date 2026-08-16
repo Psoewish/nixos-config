5db03f3f4b0d7313418da364fbc6ca81ab2a1561
psoewish <personal@psoewish.com>
2026-08-17 01:21:54 +0200

    Delete openlinkhub.nix

--- modules/system/input-devices/openlinkhub.nix
+++
@@ -1,8 +0,0 @@
-{
-  flake.modules.nixos.openlinkhub = {inputs, ...}: {
-    nixpkgs.overlays = [(inputs.openlinkhub.overlays.default or {})];
-    imports = [inputs.openlinkhub.nixosModules.openlinkhub];
-
-    services.hardware.openlinkhub.enable = true;
-  };
-}
