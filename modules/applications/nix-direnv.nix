31c1b18022d6e832a92a5343ed40595a03805aa9
psoewish <personal@psoewish.com>
2026-08-27 17:32:49 +0200

    Remove direnv hm module

--- modules/applications/nix-direnv.nix
+++ modules/applications/nix-direnv.nix
@@ -1,16 +1,11 @@
 {
   flake.modules.nixos.nix-direnv = {inputs, ...}: {
-    home-manager.sharedModules = [inputs.self.modules.homeManager.nix-direnv];
-  };
-
-  flake.modules.homeManager.nix-direnv = {inputs, ...}: {
-    imports = [(inputs.direnv-instant.homeModules.direnv-instant or {})];
+    imports = [inputs.direnv-instant.nixosModules.direnv-instant];
     programs = {
       direnv = {
         enable = true;
         silent = true;
-        nix-direnv.enable = true;
-        stdlib = ''
+        direnvrcExtra = ''
           : ''${XDG_CACHE_HOME:=$HOME/.cache}
           declare -A direnv_layout_dirs
           direnv_layout_dir() {
@@ -20,6 +15,7 @@
             )}"
           }
         '';
+        nix-direnv.enable = true;
       };
       direnv-instant.enable = true;
     };
