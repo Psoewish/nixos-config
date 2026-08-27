61832e26f4bd03628f7dc9a8ed1bcd8854ef2eb5
psoewish <personal@psoewish.com>
2026-08-27 11:46:30 +0200

    Remove pear-desktop input + hm module

--- modules/applications/pear-desktop.nix
+++ modules/applications/pear-desktop.nix
@@ -1,34 +1,5 @@
 {
-  flake.modules.nixos.pear-desktop = {inputs, ...}: {
-    home-manager.sharedModules = [inputs.self.modules.homeManager.youtube-music];
-  };
-
-  flake.modules.homeManager.youtube-music = {inputs, ...}: {
-    imports = [(inputs.pear-desktop.homeManagerModules.default or {})];
-    programs.pear-desktop = {
-      enable = true;
-      options = {
-        appVisible = true;
-        startingPage = "Home";
-        tray = true;
-      };
-      plugins = {
-        album-color-theme.enable = true;
-        blur-nav-bar.enable = true;
-        crossfade.enable = true;
-        discord = {
-          enable = true;
-          hideGitHubButton = true;
-        };
-        scrobbler = {
-          enable = true;
-          scrobblers.listenbrainz = {
-            enable = true;
-            apiRoot = "https://api.listenbrainz.org/1/";
-            # token = "config.age.secrets.listenbrainz.path";
-          };
-        };
-      };
-    };
+  flake.modules.nixos.pear-desktop = {pkgs, ...}: {
+    environment.systemPackages = [pkgs.pear-desktop];
   };
 }
