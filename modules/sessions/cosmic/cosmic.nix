e3b686fe29a8bcd7f47e7a91f6f950202de3a1b4
psoewish <personal@psoewish.com>
2026-08-29 10:41:05 +0200

    Remove some cosmic extras

--- modules/sessions/cosmic/cosmic.nix
+++ modules/sessions/cosmic/cosmic.nix
@@ -10,8 +10,6 @@
 
     environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
     environment.systemPackages = with pkgs; [
-      cosmic-ext-calculator
-      cosmic-ext-applet-caffeine
       cosmic-ext-applet-privacy-indicator
       cosmic-monitor
       loupe
