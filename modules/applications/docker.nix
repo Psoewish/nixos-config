07950aeb0f18d629e73da62514ebcb66af0b8c43
psoewish <personal@psoewish.com>
2026-09-01 14:14:53 +0200

    Fix docker reference

--- modules/applications/docker.nix
+++ modules/applications/docker.nix
@@ -12,6 +12,6 @@ toplevel @ {config, ...}: {
 
       oci-containers.backend = "docker";
     };
-    users.groups.docker.members = [toplevel.config.flake.metadata.username];
+    users.groups.docker.members = [toplevel.config.flake.metadata.primaryUser.username];
   };
 }
