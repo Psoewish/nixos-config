5a69ad5c0e50180873f5e43066eca91ee769fdfe
psoewish <personal@psoewish.com>
2026-09-04 22:06:39 +0200

    Add substituters manually

--- hosts/shared/modules/nix-settings.nix
+++ hosts/shared/modules/nix-settings.nix
@@ -12,7 +12,11 @@ toplevel @ {config, ...}: {
       settings = {
         trusted-users = ["root" toplevel.config.flake.meta.primaryUser.username];
         auto-optimise-store = true;
-        substituters = ["https://aseipp-nix-cache.global.ssl.fastly.net"];
+        substituters = [
+          "https://aseipp-nix-cache.global.ssl.fastly.net"
+          "https://attic.xuyh0120.win/lantian"
+        ];
+        trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
         experimental-features = [
           "nix-command"
           "flakes"
