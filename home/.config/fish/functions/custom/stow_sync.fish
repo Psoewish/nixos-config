b637dfcabb0e6a50f21adafdfc0d49c96716decd
psoewish <personal@psoewish.com>
2026-09-11 14:05:47 +0200

    Fix stow function

--- home/.config/fish/functions/custom/stow_sync.fish
+++ home/.config/fish/functions/custom/stow_sync.fish
@@ -2,5 +2,5 @@ function stow_sync
     set stow_source "$HOME/nixos-config/home"
     set stow_target "$HOME"
 
-    stow --dir="$stow_source" --target="$stow_target" --dotfiles --adopt -D .
+    stow --dir="$stow_source" --target="$stow_target" --dotfiles --adopt --restow .
 end
